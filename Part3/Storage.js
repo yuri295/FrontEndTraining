f75e7a74ecd9af09623c52f099b5aba70b55e5a3
yuri295 <jin_astro0302@naver.com>
2023-12-29 03:40:29 +0900

    jsData

--- Part3/Storage.js
+++ Part3/Storage.js
@@ -4,5 +4,6 @@ const user = {
   emails: 'jin_astro0302@naver.com',
 };
 
-localStorage.setItem('user', JSON.stringify(user));
-console.log(JSON.parse(localStorage.getItem('user')));
+localStorage.setItem('user', JSON.stringify(user)); //local Storage에 데이터를 저장할 때는 문자 데이터로 변환해서 저장!
+console.log(JSON.parse(localStorage.getItem('user'))); //자바 스크립트의 데이터로 변환하여 출력!
+localStorage.removeItem('user'); //local Storage에서 데이터 삭제
