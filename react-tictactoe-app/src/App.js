bec02f2193c3e0741f9c6d4250a188ef3396462c
yuri295 <jin_astro0302@naver.com>
2023-12-29 03:40:44 +0900

    TicTacToe

---
+++ react-tictactoe-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">game-info</div>
+    </div>
+  );
+}
+
+export default App;
