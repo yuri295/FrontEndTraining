91a622347bf850e134a68a608dfe25948c8dcef3
yuri295 <jin_astro0302@naver.com>
2023-12-30 17:24:09 +0900

    change class to function

--- react-tictactoe-app/src/components/Square.js
+++ react-tictactoe-app/src/components/Square.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './Square.css';
-export default class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => {
-          this.props.onClick();
-        }}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-}
+const Square = ({ onClick, value }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
