2896623e8bedb2c2a32feb509c3865ce58c0dcbc
yuri295 <jin_astro0302@naver.com>
2024-01-20 10:10:52 +0900

    category 기능 구현

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,10 +1,13 @@
 import NewsList from './components/NewsList';
 import Categories from './components/Categories';
+import { useState, useCallback } from 'react';
 const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
   return (
     <>
-      <Categories />
-      <NewsList />;
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />;
     </>
   );
 };
--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -24,8 +24,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=97f211791f5e4484aa64df69ab80a0d9'
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=97f211791f5e4484aa64df69ab80a0d9`
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -34,7 +35,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   //대기 중일 때
   if (loading) {
