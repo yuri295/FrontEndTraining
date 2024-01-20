24304d34dc28bac79152f579a493e773d74f81b4
yuri295 <jin_astro0302@naver.com>
2024-01-21 02:17:44 +0900

    SelectColor component

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import ColorBox from './components/ColorBox';
 import { ColorProvider } from './contexts/color';
+import SelectColors from './components/SelectColor';
 
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
